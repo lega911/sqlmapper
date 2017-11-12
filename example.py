173243cde9ebc3eadf0c6e38e2cd7bd01831015d
Oleg Nechaev <lega911@gmail.com>
2017-11-12 17:35:37 +0100

    fix example

--- example.py
+++ example.py
@@ -6,23 +6,27 @@ def main():
     connection = Connection(db='example', charset='utf8')
 
     with connection() as db:
-        db.aabook.add_column('id', 'INT(11)', primary=True, auto_increment=True, exist_ok=True)
-        db.aabook.add_column('name', 'VARCHAR(32)', exist_ok=True)
-        db.aabook.add_column('value', 'INT(11)', exist_ok=True)
-        db.aabook.add_column('comment', 'TEXT', exist_ok=True)
+        # add columns into table `tblname`
+        db.tblname.add_column('id', 'INT(11)', primary=True, auto_increment=True, exist_ok=True)
+        db.tblname.add_column('name', 'VARCHAR(32)', exist_ok=True)
+        db.tblname.add_column('value', 'INT(11)', exist_ok=True)
+        db.tblname.add_column('comment', 'TEXT', exist_ok=True)
 
     with connection() as db:
-        db.aabook.insert({'name': 'Ubuntu', 'value': 10, 'comment': 'Linux'})
+        # insert row into `tblname`
+        db.tblname.insert({'name': 'Ubuntu', 'value': 10, 'comment': 'Linux'})
 
-        for d in db.aabook.find({'name': 'Ubuntu'}):
+        for d in db.tblname.find({'name': 'Ubuntu'}):
             print(d)
 
-        db.aabook.update({'name': 'Ubuntu'}, {'value': 16})
+        db.tblname.update({'name': 'Ubuntu'}, {'value': 16})
 
-        d = db.aabook.find_one(1)  # get one when primary key == 1
+        # get one row where primary key == 1
+        d = db.tblname.find_one(1)
         print(d)
 
-        d = db.aabook.find_one(['value < %s and name = %s', 20, 'Ubuntu'])  # more complex filter
+        # more complex filter
+        d = db.tblname.find_one(['value < %s and name = %s', 20, 'Ubuntu'])
         print(d)
 
 
