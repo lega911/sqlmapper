bbf568929d3291a3c5802437cee3f41f5f306375
Oleg Nechaev <lega911@gmail.com>
2018-02-17 23:10:27 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='sqlmapper',
+    version='0.1',
+    description='SQL Mapper',
+    author='Oleg Nechaev',
+    author_email='lega911@gmail.com',
+    url='https://github.com/lega911/sqlmapper',
+    packages=['sqlmapper'],
+    license='MIT'
+)
